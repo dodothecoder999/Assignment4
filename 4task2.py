5c6ad773ffbb466de42e8be3e3f07a090544ed95
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 22:44:25 +0530

    Update 4task2.py

--- 4task2.py
+++ 4task2.py
@@ -17,3 +17,14 @@ reading_file=file1.read()
 print(reading_file)
 file1.close()
 
+OUTPUT
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/4task2.py
+Enter text to write to file:hello
+Data successfully written to simple.txt
+
+Enter additional text to append: lenovo
+Data successfully appended
+
+final content of simple.txt:
+hello lenovo
+
